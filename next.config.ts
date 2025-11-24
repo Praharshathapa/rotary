3c91152b96ad81ba36cb8914b0119c531bb8e7d3
Praharshathapa <thapapraharsha@gmail.com>
2025-11-25 01:37:21 +0545

    gh-page conf

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: 'export',
   generateBuildId: () => {
     return `${Date.now()}`;
   },
   images: {
     unoptimized: true,
   },
+    basePath: '/rotary',
+  assetPrefix: '/rotary'
 };
 
 export default nextConfig;
